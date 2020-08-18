4736d37c99f50a7e09d7dd7645554d63c5a412c1
rivavillageacademy <69723115+rivavillageacademy@users.noreply.github.com>
2020-08-18 19:54:53 +0400

    Add files via upload

---
+++ Multiplication Table/Multiplication Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiplication Table
+//
+//  Created by Muhamed Alkhatib on 17/08/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
